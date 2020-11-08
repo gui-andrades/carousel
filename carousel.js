3b5cfdb5cfbec375841d44ad55720d75af9f56a4
Bernola <Bernola@users.noreply.github.com>
2020-11-08 19:47:49 -0300

    Made HTML easier to setup

--- carousel.js
+++ carousel.js
@@ -1,19 +1,18 @@
 let container = document.getElementsByClassName('carousel')[0]
-let images = container.querySelector('.carousel-images')
+let items = container.querySelector('.carousel-images').children
 let sideButtons = container.getElementsByTagName('a')
 let indicators = container.querySelector('.carousel-indicators').children
-let items = images.children
 let length = items.length
-let current = -1
+let current
 
-for(let i in items) {
+for(let i = 0; i < length; i++) {
+    items[i].classList.add('carousel-item')
     if(items[i].classList.contains('active')) {
         current = i
-        break
     }
 }
 
-if (container.classList.contains('slide')){
+if(container.classList.contains('slide')){
     slideInterval = setInterval(nextCarousel, 4000)
     container.addEventListener('mouseenter', () => {
         clearInterval(slideInterval)
@@ -24,11 +23,15 @@ if (container.classList.contains('slide')){
 }
 
 if(sideButtons.length == 2){
-    var btnLeft = container.querySelector('.carousel-btn-left')
-    var btnRight = container.querySelector('.carousel-btn-right')
-    addCarouselEvents()
+    sideButtons[0].addEventListener('click', prevCarousel)
+    sideButtons[0].classList.add('carousel-btn-left')
+    sideButtons[1].addEventListener('click', nextCarousel)
+    sideButtons[1].classList.add('carousel-btn-right')
 }else{
-    container.removeChild(sideButtons[0])
+    while(sideButtons.length > 0){
+        container.removeChild(sideButtons[0])
+    }
+    sideButtons = null
 }
 
 if(indicators.length == length){
@@ -40,34 +43,29 @@ if(indicators.length == length){
         })
     }
 }else{
-    container.querySelector('.carousel-indicators').remove()
-    indicators = null
+    while(indicators.length > 0){
+        container.removeChild(indicators[0])
+    }
+    sideButtons = null
 }
 
 function setActive(n){
     if(n == current) return
-    items[current].classList.remove('active')
     items[n].classList.add('active')
     if(indicators){
-        indicators[current].classList.remove('active')
         indicators[n].classList.add('active')
+        indicators[current].classList.remove('active')
     }
+    items[current].classList.remove('active')
+    current = n
 }
-
-function addCarouselEvents(){
-    btnLeft.addEventListener('click', prevCarousel)
-    btnRight.addEventListener('click', nextCarousel)
-}
-
 
 function nextCarousel(){
     let next = (current + 1) % length
     setActive(next)
-    current = next
 }
 
 function prevCarousel(){
     let prev = (current - 1 + length) % length
     setActive(prev)
-    current = prev
 }
