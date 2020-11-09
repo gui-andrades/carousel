ffb8aa94f51527c014c9305a0521b5203d1ee8dc
Bernola <Bernola@users.noreply.github.com>
2020-11-09 00:42:14 -0300

    Changed main logic to OO so there can be more than 1 carousel in a page

--- carousel.js
+++ carousel.js
@@ -1,116 +1,145 @@
-let container = document.getElementsByClassName('carousel')[0]
-let items = container.querySelector('.carousel-images').children
-let sideButtons = container.getElementsByTagName('a')
-let indicators = container.querySelector('.carousel-indicators').children
-let length = items.length
-let current
-
-for(let i = 0; i < length; i++) {
-    items[i].classList.add('carousel-item')
-    if(items[i].classList.contains('active')) {
-        current = i
+class Carousel{
+    constructor(index){
+        this.container = containers[index]
+        this.items = this.container.querySelector('.carousel-images').children
+        this.sideButtons = this.container.getElementsByTagName('a')
+        this.indicators = this.container.querySelector('.carousel-indicators').children
+        this.length = this.items.length
+        this.current = -3
     }
-}
 
-if(container.classList.contains('slide')){
-    slideInterval = setInterval(nextCarousel, 4000)
-    container.addEventListener('mouseenter', () => {
-        clearInterval(slideInterval)
-    })
-    container.addEventListener('mouseleave', () => {
-        slideInterval = setInterval(nextCarousel, 4000)
-    })
-}
-
-if(sideButtons.length == 2){
-    sideButtons[0].addEventListener('click', prevCarousel)
-    sideButtons[0].classList.add('carousel-btn-left')
-    sideButtons[1].addEventListener('click', nextCarousel)
-    sideButtons[1].classList.add('carousel-btn-right')
-}else{
-    while(sideButtons.length > 0){
-        container.removeChild(sideButtons[0])
-    }
-    sideButtons = null
-}
-
-if(indicators.length == length){
-    indicators[current].classList.add('active')
-    for(let i = 0; i < length; i++){
-        indicators[i].addEventListener('click', () => {
-            setActive(i, '')
-            current = i
-        })
-    }
-}else{
-    while(indicators.length > 0){
-        container.removeChild(indicators[0])
-    }
-    sideButtons = null
-}
-
-function setActive(n, dir){
-    if(n == current) return
-    if(indicators){
-        indicators[n].classList.add('active')
-        indicators[current].classList.remove('active')
-    }
-    let prev = current
-    if(dir == 'right'){
-        toRight(prev, n)
-    }else if(dir == 'left'){
-        toLeft(prev, n)
-    }else if(dir == ''){
-        if(n < current){
-            toRight(prev, n)
-        }
-        if(n > current){
-            toLeft(prev, n)
+    initItems(){
+        for(let i = 0; i < this.length; i++) {
+            this.items[i].classList.add('carousel-item')
+            if(this.items[i].classList.contains('active')) {
+                this.current = i
+            }
         }
     }
-    current = n
-}
 
-function toRight(prev, n){
-    let timeR = 1
-    let transInterval = setInterval(() => {
-        items[current].style.left = `${timeR-100}%`
-        items[current].classList.add('active')
-        items[prev].style.left = `${timeR}%`
-        if(timeR >= 100){
-            clearStyle(prev)
-            clearInterval(transInterval)
+    initSlide(){
+        if(this.container.classList.contains('slide')){
+            this.slideInterval = setInterval(this.nextCarousel, 4000)
+            this.container.addEventListener('mouseenter', () => {
+                clearInterval(this.slideInterval)
+            })
+            this.container.addEventListener('mouseleave', () => {
+                this.slideInterval = setInterval(this.nextCarousel, 4000)
+            })
         }
-        timeR++
-    }, 5)
-}
+    }
 
-function toLeft(prev, n){
-    let timeL = 99
-    let transInterval = setInterval(() => {
-        items[current].style.left = `${timeL}%`
-        items[current].classList.add('active')
-        items[prev].style.left = `${timeL-100}%`
-        if(timeL <= 0){
-            clearStyle(prev)
-            clearInterval(transInterval)
+    initSideButtons(){
+        if(this.sideButtons.length == 2){
+            this.sideButtons[0].addEventListener('click', () => {
+                this.prevCarousel(this)
+            })
+            this.sideButtons[0].classList.add('carousel-btn-left')
+            this.sideButtons[1].addEventListener('click', () => {
+                this.nextCarousel(this)
+            })
+            this.sideButtons[1].classList.add('carousel-btn-right')
+        }else{
+            while(this.sideButtons.length > 0){
+                this.container.removeChild(this.sideButtons[0])
+            }
+            this.sideButtons = null
         }
-        timeL--
-    }, 5)
+    }
+
+    initIndicators(){
+        let self = this
+        if(this.indicators.length == this.length){
+            this.indicators[this.current].classList.add('active')
+            for(let i = 0; i < this.length; i++){
+                this.indicators[i].addEventListener('click', () => {
+                    this.setActive(i, '')
+                    this.current = i
+                })
+            }
+        }else{
+            while(this.indicators.length > 0){
+                this.container.removeChild(this.indicators[0])
+            }
+            this.sideButtons = null
+        }
+    }
+
+    setActive(n, dir){
+        if(n == this.current) return
+        if(this.indicators){
+            this.indicators[n].classList.add('active')
+            this.indicators[this.current].classList.remove('active')
+        }
+        let prev = this.current
+        if(dir == 'right'){
+            this.toRight(prev, n)
+        }else if(dir == 'left'){
+            this.toLeft(prev, n)
+        }else if(dir == ''){
+            if(n < this.current){
+                this.toRight(prev, n)
+            }
+            if(n > this.current){
+                this.toLeft(prev, n)
+            }
+        }
+        this.current = n
+    }
+
+    toRight(prev, n){
+        let timeR = 1
+        let transInterval = setInterval(() => {
+            this.items[this.current].style.left = `${timeR-100}%`
+            this.items[this.current].classList.add('active')
+            this.items[prev].style.left = `${timeR}%`
+            if(timeR >= 100){
+                this.clearStyle(prev)
+                clearInterval(transInterval)
+            }
+            timeR++
+        }, 5)
+    }
+
+    toLeft(prev, n){
+        let timeL = 99
+        let transInterval = setInterval(() => {
+            this.items[this.current].style.left = `${timeL}%`
+            this.items[this.current].classList.add('active')
+            this.items[prev].style.left = `${timeL-100}%`
+            if(timeL <= 0){
+                this.clearStyle(prev)
+                clearInterval(transInterval)
+            }
+            timeL--
+        }, 5)
+    }
+
+    clearStyle(prev){
+        this.items[this.current].removeAttribute('style')
+        this.items[prev].removeAttribute('style')
+        this.items[prev].classList.remove('active')
+    }
+
+    nextCarousel(self){
+        let next = (this.current + 1) % this.length
+        self.setActive(next, 'left')
+    }
+
+    prevCarousel(self){
+        let prev = (this.current - 1 + this.length) % this.length
+        self.setActive(prev, 'right')
+    }
 }
 
-function clearStyle(prev){
-    items[current].removeAttribute('style')
-    items[prev].removeAttribute('style')
-    items[prev].classList.remove('active')
-}
+let containers = document.getElementsByClassName('carousel')
+let carousels = []
 
-function nextCarousel(){
-    let next = (current + 1) % length
-    setActive(next, 'left')
-}
-
-function prevCarousel(){
-    let prev = (current - 1 + length) % length
-    setActive(prev, 'right')
+for(let init = 0; init < containers.length; init++){
+    let carousel = new Carousel(init)
+    carousel.initItems()
+    carousel.initSlide()
+    carousel.initSideButtons()
+    carousel.initIndicators()
+    carousels.push(carousel)
 }
